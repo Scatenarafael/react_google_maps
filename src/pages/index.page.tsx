329dc2c8d417224cc3127ccd50fe8abb483eb791
Scatenarafael <rafascatena@gmail.com>
2023-05-16 23:49:03 -0300

    dev: search places panTo location ok

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,29 +1,68 @@
-import { useJsApiLoader, GoogleMap, Marker, Data } from "@react-google-maps/api"
-import "../styles/Home.module.css"
+import Places from "@/components/places";
+import { LatLngLiteral, MapOptions } from "@/utils/functions";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+} from "@react-google-maps/api";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 export default function Home() {
-  const containerStyle = {
-    width: '100%',
-    height: '100vh'
-  };
-  
-  const center = {
-    lat: -22.9181601,
-    lng: -47.1263665
-  };
-  
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ? process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY : "",
-  })
+  const [office, setOffice] = useState<LatLngLiteral>()
 
-  if (!isLoaded) return <h1>Loading...</h1>
+  const mapRef = useRef<GoogleMap>();
+
+  const center = useMemo<LatLngLiteral>(
+    () => ({
+      lat: -22.9181601,
+      lng: -47.1263665,
+      altitude: 0,
+    }),
+    []
+  );
+
+  const options = useMemo<MapOptions>(
+    () => ({
+      mapId: "130dccf11925a2f7",
+      clickableIcons: false,
+      disableDefaultUI: true,
+    }),
+    []
+  );
+
+  const onLoad = useCallback((map: GoogleMap) => { return mapRef.current = map}, []);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+      ? process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+      : "",
+    libraries: ["places"],
+  });
+
+  if (!isLoaded) return <h1>Loading...</h1>;
+
 
   return (
-    <>
-      <GoogleMap zoom={14} center={center} mapContainerStyle={containerStyle}> 
-        <Marker position={center} />
-      </GoogleMap>
-    </>
-  )
+    <div className="container">
+      <div className="controls">
+        <h1>Commute?</h1>
+        <Places setOffice={(position) => {
+          setOffice(position)
+          mapRef.current?.panTo(position)
+          }} />
+      </div>
+      <div className="map">
+        <GoogleMap
+          zoom={14}
+          center={center}
+          mapContainerClassName="map-container"
+          options={options}
+          onLoad={onLoad}
+        >
+          <Marker position={office} />
+        </GoogleMap>
+      </div>
+    </div>
+  );
 }
---
+++ src/utils/functions.ts
@@ -0,0 +1,4 @@
+export type LatLngLiteral = google.maps.LatLngAltitudeLiteral;
+export type DirectionsResult = google.maps.DirectionsResult;
+export type MapOptions = google.maps.MapOptions;
+
